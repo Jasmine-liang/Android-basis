a5c566fee98e608029e4cb8e54bad7282c3490bc
Your Name <you@example.com>
2021-03-18 22:30:27 +0800

    add-app

--- README.md
+++ README.md
@@ -11,4 +11,9 @@ It lists ten affirmations that you love and displays a scrollable list of texts 
 ## Word App
 Search for a word by its first letter and switch to [Dictionary.com](https://www.dictionary.com/) to look up the meaning of the word.
 ## Unscramble App
-The game shows ten random scrambled words for the player to unscramble.You can either Skip the word or guess a word and tap Submit. If you guess correctly, the score increases. An incorrect guess shows an error state in the text field. With each new word, the word count also increases.
+"The game shows ten random scrambled words for the player to unscramble.You can either Skip the word or guess a word and tap Submit. If you guess correctly, the score increases. An incorrect guess shows an error state in the text field. With each new word, the word count also increases."
+## Cupcake App
+"This app contains an order flow for cupcakes with options for quantity, flavor, and pickup date.
+The order details get displayed on an order summary screen and can be shared to another app to send the order.
+This app demonstrates multiple fragments in an activity, a shared ViewModel across fragments,
+data binding, LiveData, and the Jetpack Navigation component."
---
+++ cupcake-app/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -0,0 +1,71 @@
+package com.example.cupcake.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import java.text.NumberFormat
+import java.util.*
+import java.text.SimpleDateFormat
+
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
+class OrderViewModel : ViewModel(){
+    private val _quantity = MutableLiveData<Int>(0)
+    val quantity: LiveData<Int> = _quantity
+
+    private val _flavor = MutableLiveData<String>("")
+    val flavor: LiveData<String> = _flavor
+
+    private val _date = MutableLiveData<String>("")
+    val date: LiveData<String> = _date
+
+    private val _price = MutableLiveData<Double>(0.0)
+    val price: LiveData<String> = Transformations.map(_price){
+        NumberFormat.getCurrencyInstance().format(it)
+    }
+
+    val dateOptions = getPickupOptions()
+    fun setQuantity(numberCupcakes: Int){
+        _quantity.value = numberCupcakes
+
+    }
+    fun setFlavor(desiredFlavor: String){
+        _flavor.value = desiredFlavor
+        updatePrice()
+    }
+    fun setDate(pickupDate: String){
+        _date.value = pickupDate
+        updatePrice()
+    }
+    fun hasNoFlavorSet(): Boolean {
+        return _flavor.value.isNullOrEmpty()
+    }
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calender = Calendar.getInstance()
+        // Create a list of dates starting with the current date and the following 3 dates
+        repeat(4){
+            options.add(formatter.format(calender.time))
+            calender.add(Calendar.DATE, 1)
+        }
+        return options
+    }
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = ""
+        _price.value = 0.0
+    }
+    init {
+        resetOrder()
+    }
+    private fun updatePrice(){
+        var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        if (dateOptions[0] == _date.value) {
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatedPrice
+    }
+}
